c1425a6851d553a23ae4f974e0a55bb72abd093a
James Ingram <jamesingram2@outlook.com>
2022-03-14 03:50:00 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const signinBtn = document.querySelector('.signinBtn');
+const signupBtn = document.querySelector('.signupBtn');
+const formBx = document.querySelector('.formBx');
+const body = document.getElementById('body');
+
+
+signupBtn.onclick = function() {
+   formBx.classList.add('active');
+   body.classList.add('active');
+}
+
+signinBtn.onclick = function() {
+   formBx.classList.remove('active');
+   body.classList.remove('active');
+}
